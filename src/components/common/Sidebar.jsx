b62e8510d783d1134be113be5cb5d08a65418814
Yahampath Chandika <yhmpth@gmail.com>
2023-11-16 18:56:57 +0530

    jj

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -76,27 +76,13 @@ function SidebarComp() {
                 Agency
               </MenuItem>
             </Link>
-            <Link to="opdTest" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "opdTest" ? "selected-menu-item" : ""
-                }
-<<<<<<< HEAD
-=======
-                icon={<FontAwesomeIcon icon={faGlobe} />}
-                onClick={() => handleMenuItemClick("opdTest")}
-              >
-                Agency
-              </MenuItem>
-            </Link>
             <Link to="createPackage" className="sidebar-link">
               <MenuItem
                 className={
-                  selectedMenuItem === "cashier" ? "selected-menu-item" : ""
+                  selectedMenuItem === "createPackage" ? "selected-menu-item" : ""
                 }
->>>>>>> f1f762de7505d4bfb6770846aa0cc5c500f5645a
                 icon={<FontAwesomeIcon icon={faUser} />}
-                onClick={() => handleMenuItemClick("opdTest")}
+                onClick={() => handleMenuItemClick("createPackage")}
               >
                 Users
               </MenuItem>
