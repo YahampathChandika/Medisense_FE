4241bb8ecfc2c0a957491b4ff8bea2f66d5c5b59
Yahampath Chandika <yhmpth@gmail.com>
2023-11-18 23:09:00 +0530

    sidebar

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -28,145 +28,148 @@ function SidebarComp() {
 
   return (
     <>
-        <Sidebar
-          collapsed={collapsed}
-          width="220px"
-          collapsedWidth="70px"
-          transitionDuration={500}
-          className="sidebar"
-        >
-          <Menu>
-            {/* <button
-              className="login-right-btn btn btn-dark collapse-btn"
-              onClick={() => setCollapsed(!collapsed)}
-            > */}
-              <FontAwesomeIcon icon={collapsed ? faBars : faBars} className="collapse-btn" onClick={() => setCollapsed(!collapsed)}/>
-            {/* </button> */}
-            <h6 className="sidebar-ctgry" style={{marginTop:'20px'}}>Menu</h6>
-            <Link to="dashboard" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "Items" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faHouse} />}
-                onClick={() => handleMenuItemClick("Items")}
-              >
-                Dashboard
-              </MenuItem>
-            </Link>
-            <Link to="gcc" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "allPatients" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faSyringe} />}
-                onClick={() => handleMenuItemClick("allPatients")}
-              >
-                Tests / Packages
-              </MenuItem>
-            </Link>
-            <Link to="addAgency" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "addAgency" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faGlobe} />}
-                onClick={() => handleMenuItemClick("addAgency")}
-              >
-                Agency
-              </MenuItem>
-            </Link>
-            <Link to="createPackage" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "createPackage" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faUser} />}
-                onClick={() => handleMenuItemClick("createPackage")}
-              >
-                Users
-              </MenuItem>
-            </Link>
-            <h6 className="sidebar-ctgry" style={{marginTop:'20px'}}>Roles</h6>
-            <Link to="dashboard" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "Items" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faBookOpen} />}
-                onClick={() => handleMenuItemClick("Items")}
-              >
-                Reception
-              </MenuItem>
-            </Link>
-            <Link to="reception" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "allPatients" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faCashRegister} />}
-                onClick={() => handleMenuItemClick("allPatients")}
-              >
-                Cashier
-              </MenuItem>
-            </Link>
-            <Link to="cashier" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "cashier" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faMoneyCheckDollar} />}
-                onClick={() => handleMenuItemClick("cashier")}
-              >
-                Accounts
-              </MenuItem>
-            </Link>
-            <Link to="opdTest" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "opdTest" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faXRay} />}
-                onClick={() => handleMenuItemClick("opdTest")}
-              >
-                X-Ray
-              </MenuItem>
-            </Link>
-            <Link to="cashier" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "cashier" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faFlaskVial} />}
-                onClick={() => handleMenuItemClick("cashier")}
-              >
-                Lab
-              </MenuItem>
-            </Link>
-            <Link to="cashier" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "cashier" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faFlask} />}
-                onClick={() => handleMenuItemClick("cashier")}
-              >
-                Mini-lab
-              </MenuItem>
-            </Link>
-            <Link to="cashier" className="sidebar-link">
-              <MenuItem
-                className={
-                  selectedMenuItem === "cashier" ? "selected-menu-item" : ""
-                }
-                icon={<FontAwesomeIcon icon={faUserDoctor} />}
-                onClick={() => handleMenuItemClick("cashier")}
-              >
-                Doctor
-              </MenuItem>
-            </Link>
-          </Menu>
-        </Sidebar>
+      <Sidebar
+        collapsed={collapsed}
+        width="220px"
+        collapsedWidth="70px"
+        transitionDuration={500}
+        className="sidebar"
+      >
+        <Menu>
+          <FontAwesomeIcon
+            icon={collapsed ? faBars : faBars}
+            className="collapse-btn"
+            onClick={() => setCollapsed(!collapsed)}
+          />
+          <h6 className="sidebar-ctgry" style={{ marginTop: "20px" }}>
+            Menu
+          </h6>
+          <Link to="dashboard" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "Items" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faHouse} />}
+              onClick={() => handleMenuItemClick("Items")}
+            >
+              Dashboard
+            </MenuItem>
+          </Link>
+          <Link to="gcc" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "Tests" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faSyringe} />}
+              onClick={() => handleMenuItemClick("Tests")}
+            >
+              Tests / Packages
+            </MenuItem>
+          </Link>
+          <Link to="addAgency" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "addAgency" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faGlobe} />}
+              onClick={() => handleMenuItemClick("addAgency")}
+            >
+              Agency
+            </MenuItem>
+          </Link>
+          <Link to="createPackage" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "createPackage" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faUser} />}
+              onClick={() => handleMenuItemClick("createPackage")}
+            >
+              Users
+            </MenuItem>
+          </Link>
+          <h6 className="sidebar-ctgry" style={{ marginTop: "20px" }}>
+            Roles
+          </h6>
+          <Link to="dashboard" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "Reception" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faBookOpen} />}
+              onClick={() => handleMenuItemClick("Reception")}
+            >
+              Reception
+            </MenuItem>
+          </Link>
+          <Link to="reception" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "allPatients" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faCashRegister} />}
+              onClick={() => handleMenuItemClick("allPatients")}
+            >
+              Cashier
+            </MenuItem>
+          </Link>
+          <Link to="cashier" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "Accounts" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faMoneyCheckDollar} />}
+              onClick={() => handleMenuItemClick("Accounts")}
+            >
+              Accounts
+            </MenuItem>
+          </Link>
+          <Link to="opdTest" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "opdTest" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faXRay} />}
+              onClick={() => handleMenuItemClick("opdTest")}
+            >
+              X-Ray
+            </MenuItem>
+          </Link>
+          <Link to="cashier" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "Lab" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faFlaskVial} />}
+              onClick={() => handleMenuItemClick("Lab")}
+            >
+              Lab
+            </MenuItem>
+          </Link>
+          <Link to="cashier" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "Mini-lab" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faFlask} />}
+              onClick={() => handleMenuItemClick("Mini-lab")}
+            >
+              Mini-lab
+            </MenuItem>
+          </Link>
+          <Link to="cashier" className="sidebar-link">
+            <MenuItem
+              className={
+                selectedMenuItem === "Doctor" ? "selected-menu-item" : ""
+              }
+              icon={<FontAwesomeIcon icon={faUserDoctor} />}
+              onClick={() => handleMenuItemClick("Doctor")}
+            >
+              Doctor
+            </MenuItem>
+          </Link>
+        </Menu>
+      </Sidebar>
     </>
   );
 }
