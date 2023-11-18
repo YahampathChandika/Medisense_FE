aa4f9c85de320bfb315828b79acf3923a6cec7d6
Yahampath Chandika <yhmpth@gmail.com>
2023-11-18 20:29:56 +0530

    ss

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -39,6 +39,7 @@ const Navbar = () => {
                       src={user.imageUrl}
                       alt=""
                     />
+                    <p className="text-xl font-bold text-white ml-5">Admin</p>
                   </Menu.Button>
                 </div>
                 <Transition
@@ -50,7 +51,7 @@ const Navbar = () => {
                   leaveFrom="transform opacity-100 scale-100"
                   leaveTo="transform opacity-0 scale-95"
                 >
-                  <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Items className="absolute  z-50 mt-2 w-40 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <Menu.Item>
                       {({ active }) => (
                         <Link
